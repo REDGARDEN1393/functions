05c67a7fce36699a63ee5e0f8b0cbdc685b89fd4
МУРОДЖОН <@DESKTOP-ITKOMRT>
2023-02-14 19:16:23 +0500

    Функции

--- Source.cpp
+++ Source.cpp
@@ -3,16 +3,22 @@
 #include<ctime>
 
 //Процедурная функция
-void say_hello() {
+void say_hello() {// void Функция не возвращающая никаких значений и выводящая сообщения
 	std::cout << "Hello world!\n";
+	std::cout << "Bay world!\n";
+}
+void print_sum(int num1,int num2) {//   print_sum Функция не возвращающая никаких значений,но принимающая два числа и выводащая их сумму
+	std::cout << num1 << " + " << num2 << " = " << num1 + num2 << "\n";
 }
 int main() {
 	setlocale(LC_ALL, "rus");
-	int n;
-	say_hello()
-
-
-
+	int n, m;
+	say_hello();
+	say_hello();
+	std::cout << "Введите два числа -> ";
+	std::cin >> n >> m;
+	print_sum(n, m);
+	print_sum(10, 7);
 
 
 
