cdba51165b30edd25e500fcc3803c14635ff3b69
МУРАТ <@DESKTOP-ITKOMRT>
2023-02-14 18:55:57 +0500

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+//Процедурная функция
+void say_hello() {
+	std::cout << "Hello world!\n";
+}
+int main() {
+	setlocale(LC_ALL, "rus");
+	int n;
+	say_hello()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+
+}
